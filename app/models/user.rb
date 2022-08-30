f5f1fe79e7eac71aa267dddf3832bfaff62ccb7b
Joff317 <j.freixanet@hotmail.com>
2022-08-30 14:51:14 +0200

    secure commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
 
   validates :email, presence: true 
   has_many :articles 
+  has_many :comments
 
   devise :database_authenticatable, :registerable,
   :jwt_authenticatable,
